dac59cbf7875a485bb10b1996cea315f95eaf044
luizfloureiro <45314833+luizfloureiro@users.noreply.github.com>
2019-06-30 23:13:44 -0300

    Implementação da remoção por categoria

--- cardapio_pizza/arvore_b_mais.c
+++ cardapio_pizza/arvore_b_mais.c
@@ -896,3 +896,34 @@ TPizza** busca_por_categoria(int d, char *nome_arquivo_dados, char *categoria) {
 
     return resultado;
 }
+
+void remove_por_categoria(int d, char *nome_arquivo_dados, char *nome_arquivo_indices, char *nome_arquivo_metadados, char *categoria) {
+
+    // ABRE OS ARQUIVOS
+    FILE *arq_dados = fopen(nome_arquivo_dados, "rb");
+
+    // INICIALIZA VARIÁVEIS
+    // POSIÇÃO DO PRIMEIRO NÓ FOLHA (NÓ QUE POSSUI O MENOR CÓDIGO)
+    int prim_folha = busca(0, nome_arquivo_metadados, nome_arquivo_indices, nome_arquivo_dados, d);
+
+    fseek(arq_dados, prim_folha, SEEK_SET);
+    TNoFolha* noFolha = le_no_folha(d, arq_dados);
+
+    // PERCORRE OS NÓS FOLHAS
+    while(noFolha->pont_prox != -1) {
+
+        // PERCORRE AS PIZZAS DO NÓ FOLHA PROCURANDO AS QUE TEM A CATEGORIA A SER REMOVIDA
+        for(int i = 0; i < noFolha->m; i++) {
+            if (strcmp(noFolha->pizzas[i]->categoria))
+                exclui(noFolha->pizzas[i]->cod, nome_arquivo_metadados, nome_arquivo_indices, nome_arquivo_dados, d);
+        }
+
+        fseek(arq_dados, noFolha->pont_prox, SEEK_SET);
+        noFolha = le_no_folha(d, arq_dados);
+
+    }
+
+    // FECHA O ARQUIVO DE DADOS E LIBERA O NÓ FOLHA
+    fclose(arq_dados);
+    libera_no_folha(d, noFolha);
+}
